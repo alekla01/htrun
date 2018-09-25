b038a3cd8147dd1be661b4db4195b1161d66d1f3
alekla01 <aleksi.klasila@arm.com>
2018-09-25 08:47:03 +0300

    edit

--- mbed_host_tests/host_tests_conn_proxy/conn_primitive_remote.py
+++ mbed_host_tests/host_tests_conn_proxy/conn_primitive_remote.py
@@ -79,7 +79,9 @@ class RemoteConnectorPrimitive(ConnectorPrimitive):
         # Remote DUT connection, flashing and reset...
         try:
             self.__remote_connect(baudrate=self.baudrate)
+            self.__remote_disconnect()
             self.__remote_flashing(self.image_path, forceflash=True)
+            self.__remote_connect(baudrate=self.baudrate)
         except Exception as error:
             self.logger.prn_err(str(error))
             self.__remote_release()
